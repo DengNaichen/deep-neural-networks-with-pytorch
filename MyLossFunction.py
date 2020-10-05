a18db3673c1145f5032f4424eb0cc1ad7c6bb47b
DengNaichen <41554700+DengNaichen@users.noreply.github.com>
2020-10-05 12:05:34 -0400

    10.5

--- MyLossFunction.py
+++ MyLossFunction.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import torch
 import numpy as np
 from numpy import sin, pi
@@ -17,7 +17,8 @@ x: input of NN
 
 def cross_entropy_loss(yhat, y):
     L = len(yhat)
-    loss = - (1 / L) * (torch.mm(y.T, torch.log(yhat)) # here we don't need 2pi, but we need to divide 2pi when calculate the integration
+    loss = - (1 / L) * (torch.mm(y.T, torch.log(yhat))  # here we don't need 2pi, but we need to divide 2pi when
+                        # calculate the integration
                         + torch.mm((1 - y).T, torch.log(1 - yhat)))
     return loss
 
@@ -30,14 +31,11 @@ def mse_loss(yhat, y):
 
 def free_energy(yhat, x, matrix, rho, lambd):
     L = len(yhat)
-    first_term = (2 * pi / L) * (torch.mm(yhat.T, torch.log(rho * yhat)))
-    second_term = (2 * pi**2 * rho / L ** 2) * torch.mm(torch.mm(yhat.T, matrix), yhat)  # todo
+    first_term = (2 * pi / L) * (torch.mm(yhat.T, torch.log(rho * yhat)))  # todo, find another way
+    second_term = (2 * pi ** 2 * rho / L ** 2) * torch.mm(torch.mm(yhat.T, matrix), yhat)  # todo
     third_term = lambd * torch.square(torch.trapz(yhat.T, x.T) - 1)  # the only reason we need x
     loss = first_term \
-         + second_term \
-         + third_term
-    
+           + second_term \
+           + third_term
 
     return loss
-
-
--- Train.py
+++ Train.py
@@ -3,17 +3,6 @@ from MyLossFunction import cross_entropy_loss, free_energy
 import torch
 
 
-def lr_decay(epoch_num, init_lr, decay_rate):
-    '''
-    :param init_lr: initial learning rate
-    :param decay_rate: if decay rate = 1, no decay
-                       for epoch = 10000, I would use 0.9996 or 0.9997
-    :return: learning rate
-    '''
-    lr_1 = init_lr * decay_rate ** epoch_num
-    return lr_1
-
-
 def fit(x, y, net, epochs, init_lr, decay_rate, diagram=False):
     if diagram == True:
         plt.ion()
@@ -22,7 +11,7 @@ def fit(x, y, net, epochs, init_lr, decay_rate, diagram=False):
         ax2 = fig.add_subplot(2, 1, 2)
         fig.subplots_adjust(left=0.15, bottom=0.1, top=0.9, right=0.95, hspace=0.4, wspace=0.25)
 
-    loss_points = []    
+    loss_points = []
     for i in range(epochs):
         lr_1 = lr_decay(i, init_lr, decay_rate)
         optimizer = torch.optim.Adam(net.parameters(), lr=lr_1)
@@ -33,12 +22,12 @@ def fit(x, y, net, epochs, init_lr, decay_rate, diagram=False):
         loss.backward()
         optimizer.step()
 
-        if i % 10 == 0 and diagram == True: 
+        if i % 10 == 0 and diagram == True:
             ax1.cla()
             ax2.cla()
             ax1.scatter(x.data.numpy(), y.data.numpy(), marker=".")
             ax1.plot(x.data.numpy(), yhat.data.numpy(), 'r-', lw=1)
-            ax1.set_xlabel (" \\theta")
+            ax1.set_xlabel(" \\theta")
             ax2.set_xlim([0, epochs])
             ax2.text(150, 0.5, 'Loss=%.5f' % loss.data.numpy(), fontdict={'size': 10, 'color': 'black'})
             ax2.text(300, 0.5, 'epochs=%i' % i, fontdict={'size': 10, 'color': 'blue'})
@@ -52,7 +41,7 @@ def fit(x, y, net, epochs, init_lr, decay_rate, diagram=False):
     return net
 
 
-def train_free_energy(net, x, rho, lambd, optimizer, matrix, epochs, diagram= True):
+def train_free_energy(net, x, rho, lambd, optimizer, matrix, epochs, diagram=True):
     if diagram:
         plt.ion()
         fig = plt.figure()
@@ -78,8 +67,8 @@ def train_free_energy(net, x, rho, lambd, optimizer, matrix, epochs, diagram= Tr
             ax2.set_ylabel("$f$")
             ax1.set_title("Distribution Function")
             ax2.set_xlim([0, epochs])
-            ax2.text(epochs/4, 1.1, 'Loss=%.7f' % loss.data.numpy(), fontdict={'size': 10, 'color': 'black'})
-            ax2.text(epochs/1.5, 1.1, 'epochs=%i' % i, fontdict={'size': 10, 'color': 'blue'})
+            ax2.text(epochs / 4, 1.1, 'Loss=%.7f' % loss.data.numpy(), fontdict={'size': 10, 'color': 'black'})
+            ax2.text(epochs / 1.5, 1.1, 'epochs=%i' % i, fontdict={'size': 10, 'color': 'blue'})
             ax2.set_xlabel("epochs")
             ax2.set_ylabel("Loss(Free Energy)")
             ax2.set_title("Free Energy with $\\rho =$ {} and $\\lambda = $ {}".format(rho, lambd))
@@ -91,24 +80,25 @@ def train_free_energy(net, x, rho, lambd, optimizer, matrix, epochs, diagram= Tr
 
     return net
 
+
 # why we need tensor data: we only want to use the "tensor" data in tensor without gradient data,
 # .data can do this work
 
 
-def train_free_energy_two_inputs(net, x, inputs, rho, lambd, optimizer, matrix, epochs, diagram=True):
-
-    '''
+def train_free_energy_two_inputs(net, x, inputs, rho, lambd, scheduler, optimizer, matrix, epochs, diagram=True):
+    """
     :param net: NN with random parameters
     :param x: an array, from 0 to 2pi
     :param inputs: inputs is an matrix with dimension (len(x), 2), sin(x) and cos(x)
     :param rho: constant
     :param lambd: penalty norm
-    :param optimizer:
+    :param optimizer: the optimizer
+    :param scheduler: schedule learning rate
     :param matrix: |sin(theta - theta')|
     :param epochs: number of iterations
     :param diagram: boolean, weather to plot (and save) the diagram
     :return: NN with trained parameters
-    '''
+    """
 
     plt.ion()
     fig = plt.figure()
@@ -117,24 +107,24 @@ def train_free_energy_two_inputs(net, x, inputs, rho, lambd, optimizer, matrix, 
     fig.subplots_adjust(left=0.15, bottom=0.1, top=0.9, right=0.95, hspace=0.4, wspace=0.25)
     loss_points = []
 
-
     for i in range(epochs):
-        yhat = net(inputs)                               # the different from previous one
+        yhat = net(inputs)  # the different from previous one
         loss = free_energy(yhat, x, matrix, rho, lambd)  # but here we still need x, rather than input (two units)
         loss_points.append(loss.item())
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
         if i % 50 == 0:
-            #plot the output
+            # plot the output
             ax1.cla()
             ax1.plot(x.data.numpy(), yhat.data.numpy())
             ax1.set_xlabel("$\\theta$")
             ax1.set_ylabel("$f$")
             ax1.set_title("Distribution Function")
-            #plot the cost
+            # plot the cost
             ax2.cla()
             ax2.set_xlim([0, epochs])
             ax2.text(epochs / 4, 1.1, 'Loss=%.7f' % loss.data.numpy(), fontdict={'size': 10, 'color': 'black'})
@@ -149,7 +139,6 @@ def train_free_energy_two_inputs(net, x, inputs, rho, lambd, optimizer, matrix, 
     if diagram:
         plt.ioff()
         plt.show()
-        plt.savefig('figure/'+'rho=' + str(rho) + "lambda=" + str(lambd) + '.pdf')
-
+        plt.savefig('figure/' + 'rho=' + str(rho) + "lambda=" + str(lambd) + '.pdf')
 
     return net
